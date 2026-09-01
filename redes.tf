ee666288c3606c5a6f06faff6ce6892b7a715d67
guilhermef-roveri <guilherme.roveri@sptech.school>
2026-09-01 18:17:20 -0300

    Feat: Associacao das tabelas de rotas e finalizacao da config de redes

--- redes.tf
+++ redes.tf
@@ -117,3 +117,30 @@ resource "aws_route_table" "private" {
     Name = "projeto-route-table-private"
   }
 }
+
+
+#Associacao das Subnets com as Route Tables
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.private.id
+}
