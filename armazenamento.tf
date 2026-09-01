d224f407b25c107a139e4071207daaee723cc9e8
guilhermef-roveri <guilherme.roveri@sptech.school>
2026-09-01 19:39:09 -0300

    Fix: nome dos buckets

--- armazenamento.tf
+++ armazenamento.tf
@@ -1,12 +1,16 @@
 #Criacao dos S3 para analise de dados
 resource "aws_s3_bucket" "bronze_bucket" {
-  bucket = "cris-utilidades-bronze"
+  bucket = "cris-utilidades-analise-bronze"
 }
 
 resource "aws_s3_bucket" "silver_bucket" {
-  bucket = "cris-utilidades-silver"
+  bucket = "cris-utilidades-analise-silver"
 }
 
 resource "aws_s3_bucket" "gold_bucket" {
-  bucket = "cris-utilidades-gold"
+  bucket = "cris-utilidades-analise-gold"
+}
+
+resource "aws_s3_bucket" "gold_bucket" {
+  bucket = "cris-utilidades-bucket"
 }
